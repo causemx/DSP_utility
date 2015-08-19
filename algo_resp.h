11251c60bb64358f3fe5b30959072a91dc07f40d
causemx <allen.cause@gmail.com>
2015-08-19 18:49:07 +0800

    re-construct code to algo_resp.

--- algo_resp.h
+++ algo_resp.h
@@ -1,6 +1,20 @@
 #ifndef ALGO_RESP_H
 #define ALGO_RESP_H
 
+float *vector(long, long);
+int *ivector(long, long);
+float **matrix(long, long, long, long);
+
+void free_vector();
+void free_ivector();
+void free_matrix();
+
+void lubksb(float **a, int n, int *indx, float b[]);
+void ludcmp(float **a, int n, int *indx, float *d);
+
+void savgol(float *c, int np, int nl, int nr, int ld, int m);
+float *convolve(float *A, float *B, int lenA, int lenB, int *lenC);
+
 double * fir_coef(unsigned, double, int);
 int filter(int, float *, float *, int, float *, float *);
 
